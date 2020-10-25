ea8ec05d9f73f3a4aedd1b407fb4ce567cd4c606
Liubomyr Izdepskyi <41821541+Liubomyr17@users.noreply.github.com>
2020-10-25 22:46:51 +0200

    Add files via upload

---
+++ src/com/company/Solution.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+public class Solution {
+    public static void main(String[] args) {
+        final int X = 10;
+        System.out.println(X);
+     }
+}
+
+class Test {
+    public static final int CONSTANT = 0;
+
+    }
+
